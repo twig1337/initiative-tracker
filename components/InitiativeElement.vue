1e05bfe0e10bcf3dc134d5458d10366879dd3243
Thomas Ellsworth <tellsworth@rxsavingssolutions.com>
2021-08-04 16:24:10 -0500

    Styling

---
+++ components/InitiativeElement.vue
@@ -0,0 +1,28 @@
+<template>
+  <tr>
+    <td class="borderless">
+      <v-text-field label="Name" :value="name" />
+    </td>
+    <td class="borderless">
+      <v-text-field type="number" label="Initiative" :value="initiative" />
+    </td>
+    <td class="borderless">
+      <v-text-field type="number" label="Hit Points" :value="hp" />
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "InitiativeElement",
+  props: {
+    name: { type: String, default: '' },
+    initiative: { type: Number, default: null },
+    hp: { type: Number, default: null }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/InitiativeStack.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <v-simple-table id="initiative-tracker-table">
+      <template #default>
+        <thead>
+          <tr>
+            <th>
+
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+        <InitiativeElement
+            class="borderless"
+            v-for="(initiativeElement, index) in initiativeElements"
+            :key="initiativeElement.id"
+            :name="initiativeElement.name"
+            :initiative="initiativeElement.initiative"
+            :hp="initiativeElement.hp"
+            @remove="initiativeElements.splice(index, 1)"
+        />
+        </tbody>
+        <v-btn
+            text
+            fab
+            @click="addInitiativeElement"
+            class="mt-1"
+        >
+          <v-icon align-self="center">mdi-plus</v-icon>
+        </v-btn>
+      </template>
+    </v-simple-table>
+  </div>
+</template>
+
+<script>
+import InitiativeElement from "./InitiativeElement"
+
+export default {
+  name: "InitiativeStack",
+  components: { InitiativeElement },
+  data () {
+    return {
+      nextInitiativeElement: 3,
+      initiativeElements: [{id: 1}, {id: 2}]
+    }
+  },
+  methods: {
+    addInitiativeElement () {
+      this.initiativeElements.push({
+        id: this.nextInitiativeElement++
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
